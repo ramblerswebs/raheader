30968a0eb8a5f3eff683ae03834e426278abf5de
Chris Vaughan <chris@cevsystems.co.uk>
2014-10-06 08:06:11 +0100

    correction to remove title if blank

--- tmpl/default.php
+++ tmpl/default.php
@@ -48,8 +48,9 @@ if ($image != '') {
 // Title
 $website_title = $params->get('website_title');
 $title_color = getColor($params->get('title_color'), $params->get('title_color_value'));
+if ($website_title != '') {
 $header.='<h1 id="ramblerstitle" style="color:' . $title_color . '" >' . $website_title . '</h1>';
-
+}
 //SubTitle
 $website_subtitle = $params->get('website_subtitle');
 $subtitle_color = getColor($params->get('subtitle_color'), $params->get('subtitle_color_value'));
